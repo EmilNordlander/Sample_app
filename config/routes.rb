6a441c20534f0cdb28820187de49b29142dea3a8
Emil Nordlander <emil.nordlander@live.se>
2022-09-01 13:39:42 +0200

    Strukturerat kod genom render,lagt tillnew user...

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
  
+  
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'static_pages/home'
+  get '/home', to: 'static_pages#home'
+  # get 'static_pages/help' 
+  get '/help', to: 'static_pages#help'
+  # get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  # get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
